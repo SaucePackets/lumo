36cfaa60444aa7a466b6cf8ad8922953158558ba
Jerry Kingsbury <jerrykingsbury@Jerrys-MacBook-Pro.local>
2025-09-18 20:28:59 -0500

    Implement BDK Wallet Persistance using SQLite
    - Add BDKStore for managing SQLite connections
    - Update wallet creation to use PersistedWallet
    - Add test to verify persistance works across restarts

---
+++ rust/src/bdk_store.rs
@@ -0,0 +1,35 @@
+use std::path::PathBuf;
+
+use bdk_wallet::Wallet;
+use lumo_common::consts::ROOT_DATA_DIR;
+use crate::wallet::{WalletId, error::WalletError};
+use lumo_types::Network;
+use eyre::{Context, Result};
+
+pub struct BDKStore {
+    id: WalletId,
+    network: Network,
+    pub conn: bdk_wallet::rusqlite::Connection,
+}
+
+fn sqlite_data_path(wallet_id: &WalletId) -> PathBuf {
+    let db = format!("bdk_wallet_sqlite_{}.db", wallet_id.to_string().to_lowercase());
+    ROOT_DATA_DIR.join(db)
+}
+
+impl BDKStore {
+    pub fn try_new(id: &WalletId, network: impl Into<Network>) -> Result<Self> {
+        // Create database file path
+        let sqlite_data_path = sqlite_data_path(id);
+
+        // Open connection to the database
+        let conn = bdk_wallet::rusqlite::Connection::open(&sqlite_data_path)
+            .context("unable to open rusqlite connection")?;
+
+        Ok(Self {
+            id: id.clone(),
+            network: network.into(),
+            conn,
+        })
+    }
+}
