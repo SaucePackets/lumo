608188c9648ccb31907d638feafc3fca28220f91
Jerry Kingsbury <jerrykingsbury@Jerrys-MacBook-Pro.local>
2025-09-22 16:30:21 -0500

    Address Management
    Added address cycling, get unused addresses, update get new address

--- rust/crates/lumo-types/src/lib.rs
+++ rust/crates/lumo-types/src/lib.rs
@@ -3,7 +3,7 @@ pub mod amount;
 pub mod network;
 pub mod transaction;
 
-pub use address::{validate_address, Address, AddressError, AddressWithNetwork};
+pub use address::{validate_address, Address, AddressError, AddressInfo, AddressWithNetwork};
 pub use amount::Amount;
 pub use network::Network;
 pub use transaction::{Transaction, TransactionDetails};
